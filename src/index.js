5bcf2a1c49527d58edaaa997bad7e217a29b8220
root <root@DESKTOP-GNBQSCC.localdomain>
2018-05-10 23:01:16 +0200

    end of section 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDf2gGk1H2KfKQbtOAtNdSbQOj_Q8V1lt0';
+
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
